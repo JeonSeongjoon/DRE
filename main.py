bb856de82a468f18ea15d36cdca60f1f4b215519
JeonSeongjoon <02seongjoom@gmail.com>
2025-07-27 13:25:53 +0900

    TI_v1

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ model = get_peft_model(model, LoRAConfig)
 #########################################Fine-tuning#################################################
 
 #FT_data loading & sampling
-num_samples = 300
+num_samples = 3000
 data = loadnSampling(DATASET_PATH, num_samples=num_samples)
 
 #FT_preprocessing
